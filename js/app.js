a29e2d452e522d6a55816b4cc4fbab91687820e7
MARIO ACEVEDO <JUN_ACEVEDO415@YAHOO.COM>
2022-05-23 22:17:50 -0700

    added a few things

--- js/app.js
+++ js/app.js
@@ -1,4 +1,7 @@
 'use strict';
+
+
+//deleted a few things
 console.log('Anyone else think "salmon cookies" sound gross?');
 
 let storeHours = ['6:00am','7:00am','8:00am','9:00am','10:00am','11:00am','12:00pm','1:00pm','2:00pm','3:00pm','4:00pm','5:00pm','6:00pm','7:00pm'];
